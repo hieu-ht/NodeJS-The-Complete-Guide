9ce1ec22c97d6426fcd311c007ab79413d213f18
hieudevx <hoangtrunghieu10licbn@gmail.com>
2020-09-15 08:53:11 +0700

    introduction first commit

---
+++ introduction/ts/first-app.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+var fs = require("fs");
+fs.writeFileSync("hello.txt", "Hello from Node.js typescript");
---
+++ introduction/ts/first-app.ts
@@ -0,0 +1,3 @@
+import * as fs from "fs";
+
+fs.writeFileSync("hello.txt", "Hello from Node.js typescript");
