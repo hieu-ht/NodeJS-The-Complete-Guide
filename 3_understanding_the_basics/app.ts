3e2d04857c871c15fae11876cba6102b9bd55053
hieudevx <hoangtrunghieu10licbn@gmail.com>
2020-09-16 00:53:31 +0700

    [3] request response stream buffer

---
+++ 3_understanding_the_basics/app.ts
@@ -0,0 +1,56 @@
+import * as http from "http";
+import * as fs from "fs";
+
+const server = http.createServer(
+  (request: http.IncomingMessage, response: http.ServerResponse) => {
+    // console.log(request);
+    // console.log("url: ", request.url);
+    // console.log("method: ", request.method);
+    // console.log("headers: ", request.headers);
+
+    const url = request.url;
+    const method = request.method;
+
+    if (url === "/") {
+      response.setHeader("Content-Type", "text/html");
+      response.write("<html>");
+      response.write("<head><title>Enter message</title></head>");
+      response.write(
+        "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>"
+      );
+      response.write("</html>");
+      return response.end();
+    }
+
+    if (url === "/message" && method === "POST") {
+      const body = [];
+
+      request.on("data", (chunk) => {
+        console.log("chunk: ", chunk);
+        body.push(chunk);
+      });
+
+      request.on("end", () => {
+        const parsedBody = Buffer.concat(body).toString();
+        const message = parsedBody.split("=")[1];
+
+        console.log("parsedBody: ", parsedBody);
+        fs.writeFileSync("message.txt", message);
+      });
+      // fs.writeFileSync("message.txt", "DUMMY");
+      response.statusCode = 302;
+      response.setHeader("Location", "/");
+      return response.end();
+    }
+
+    response.setHeader("Content-Type", "text/html");
+    response.write("<html>");
+    response.write("<head><title>My first page</title></head>");
+    response.write("<body><h1>Hello from my Node.js</h1></body>");
+    response.write("</html>");
+    response.end();
+    // process.exit();
+  }
+);
+
+server.listen(3000);
